b3ece8b611fcf7900c2868820aa12cde17d5c772
HamidBahrami23 <hamid.bahrami42@gmail.com>
2025-11-28 17:32:36 +0330

    second simple project added, im realy excited. I used Vim

---
+++ 01_formatted_print/formatted_print/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("I used Vim Editor for Write this code");
+    let osn = "Arch";
+    println!("And remember I use {} BTW xd", osn);
+    let a = 5 ;
+    println!("a is an input and its number is {}",a);
+    println!("***********************************");
+    println!("in this part I will practice using 2 variables but repeatly in text");
+    let name = "Hamid";
+    let time = 1;
+    println!("my name is :{0} and this is not my {1} project in Rust. {1} thing that I like is be very active on git.\n I used {1} variable 3 times",name , time );
+}
---
+++ 01_formatted_print/formatted_print/target/debug/formatted_print.d
@@ -0,0 +1,1 @@
+/home/hamid/git-projects/rust/01_formatted_print/formatted_print/target/debug/formatted_print: /home/hamid/git-projects/rust/01_formatted_print/formatted_print/src/main.rs
