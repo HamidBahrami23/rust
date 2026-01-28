e9291f9cbd737d59f33266c2b32031459e9f0847
HamidBahrami23 <hamid.bahrami42@gmail.com>
2026-01-28 23:49:57 +0100

    cargo doc test

---
+++ 25_generating_doc/src/main.rs
@@ -0,0 +1,16 @@
+///A Favorate Color
+enum Color {
+    Red,
+    Blue,
+}
+
+///A piece of mail.
+struct Mail {
+    address: String,
+}
+
+///adds two numbers together
+fn add(a: i32 , b: i32) -> i32 {
+    a + b
+}
+fn main() {}
