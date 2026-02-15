a7595f145073efe07945f7f4ad2ad7cb2f057347
HamidBahrami23 <hamid.bahrami42@gmail.com>
2026-02-16 00:26:41 +0100

    I Just runed and debug it in Vim and linux terminal XD

--- 37_hashmap_5th_try/src/main.rs
+++ 37_hashmap_5th_try/src/main.rs
@@ -1,18 +1,29 @@
+use std::collections::HashMap;
 
-struct age {
+#[derive(Debug)]
+enum LangLevel {
+    A1,
+    A2,
+    B1,
+    B2,
+    C1,
+    C2,
+}
+struct Age {
     age: u8,
+    weight: f32,
+    language_level:LangLevel,
 }
 
 
 
 fn main() {
-    let mut names = hashmap::new();
-    names.insert("Sara", age {age : 34 } );
+    let mut names = HashMap::new();
+    names.insert("Sara", Age{ age: 34 ,  weight:53.0, language_level: LangLevel::A2,} );
+    names.insert("Linus", Age {age: 47 , weight: 76.2 , language_level: LangLevel::C2 ,});
 
-
-
-    match i in names("Sara") {
-        Some(num) => println!("the age of sara is {:?}",num.age ),
+    match names.get("Linus") {
+        Some(num) => println!("the age of Linus is {:?} , and the weight is : {:?} , and also his Language LVL is : {:?}",num.age , num.weight , num.language_level ),
         None => println!("user not found!")
     }
 }
