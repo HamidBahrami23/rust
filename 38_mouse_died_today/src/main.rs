bd7ca0828f5f67f2db5e79e9cbb7f7eeb0eb383a
HamidBahrami23 <hamid.bahrami42@gmail.com>
2026-02-28 23:35:09 +0100

    This is a message project

---
+++ 38_mouse_died_today/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Today is Big Day for me");
+    println!("The reason og ruining my life");
+    println!("The Mouse [Moosh-Ali] killed");
+    println!("Thanks President Trump and BB");
+    println!("In bottom of my Heart");
+    println!("Thank you");
+    println!("2026.02.28");
+    println!("Hamid Bahrami");
+}
